98cb5cc654d7123f2881e987785117d049445876
Victoria Bocharova <v.s.bocha@gmail.com>
2021-07-17 21:32:18 +0300

    Changed new Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 917_000;
-        int bonusMile = 20;
 
-       int mile = amount / bonusMile;
+
+       int mile = amount / 20;
         System.out.println(mile);
 
     }
